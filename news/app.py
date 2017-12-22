0cf7d3ff9e3bf2d2635eba7d0c6564356dd1721b
503924173 <503924173@qq.com>
2017-12-22 20:51:06 +0800

    challenge6

--- news/app.py
+++ news/app.py
@@ -7,21 +7,23 @@ app.config['TEMPLATES_AUTH_RELOAD']=True
 
 @app.route('/')
 def index():
-    filedic={}
+    newfilelst=[]
     for  _,_,filelst in os.walk('/home/shiyanlou/files'):
         for file in filelst:
-            with open('/home/shiyanlou/files/'+ file,'r') as f:
-                dic_file=json.loads(f.read())
-            filedic.update(dic_file)
-
-    return render_template('/home/shiyanlou/templates/file.html',filedic=filedic)
+                with open('/home/shiyanlou/files/'+ file,'r') as f:
+                    dic_file=json.loads(f.read())
+                    newfilelst.append(dic_file['title'])
+    return render_template('index.html',newfilelst=newfilelst)
 
 @app.route('/files/<filename>')
 def file(filename):
-    if os.path.exit(filename) :
+    filename='/home/shiyanlou/files/'+ str(filename) + '.json'
+    print(filename)
+    if os.path.exists(filename) :
+        print("ok")
         with open(filename,'r') as f:
             dic_file=json.loads(f.read())
-        return render_tempate('file.html',filedic=filedic)
+        return render_template('file.html',dic_file=dic_file)
     else:
         return render_template('404.html')
 @app.errorhandler(404)
