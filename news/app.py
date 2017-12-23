b345288e360b090feaafb8d9af811d85c1e29bcf
503924173 <503924173@qq.com>
2017-12-23 23:02:23 +0800

    challenge7

--- news/app.py
+++ news/app.py
@@ -1,10 +1,32 @@
 from flask import render_template,Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import json
 import os
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTH_RELOAD']=True
-
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root@localhost/shiyanlou'
+db=SQLAlchemy(app)
+class File(db.Model):
+    __tablename__='file'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(80))
+    created_time=db.Column(db.DateTime)
+    category_id=db.Column(db.Integer,db.ForeignKey('category.id'))
+    category=db.relationship('Category',backref=db.backref('files'))
+    content=db.Column(db.Text)
+    def __init__(self):
+        pass
+    def __repr__(self):
+        pass
+class Category(db.Model):
+    __tablename__='category'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80))
+    def __init__(self,name):
+        self.name=name
+    def __repr__(self):
+        return "<Caegory%r>"%self.name
 @app.route('/')
 def index():
     newfilelst=[]
